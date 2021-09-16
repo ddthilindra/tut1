d6bb2f47e416f8c0cffc3a571803e6b3101bf78c
ddthilindra <ddthilindra@gmail.com>
2021-09-16 16:40:48 +0530

    3rd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import TestProject from './src/main';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TestProject);
